c5a3197f6a0382d5de4d36f1fbe36c96371d840d
Ramsheed K K <ramsheedkk06@gmail.com>
2023-07-21 22:21:10 +0530

    express server commit

--- expressServer/app.js
+++ expressServer/app.js
@@ -2,14 +2,8 @@ var express=require('express')
 var app=express()
 var ejs=require('ejs')
 app.set('view engine','ejs')
-var db=require('./confi/connection')
 userRouter=require('./routes/users')
 adminRouter=require('./routes/admin')
-db.connect((err)=>{
-    if(err) console.log('error')
-    else  console.log('database conectes');
-  
-})
 app.use('/',userRouter)
 app.use('/admin',adminRouter)
 app.listen(3000,()=>{
